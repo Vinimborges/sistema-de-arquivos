71867acb9e5b2b1ea7ced961603c4698d37999ce
Vinícius Martins Borges <vinimborges@gmail.com>
2024-12-17 12:58:22 -0300

    Touch adicionando o Inode no diretório pai

--- manipulacaoArquivos/blocos.py
+++ manipulacaoArquivos/blocos.py
@@ -1,7 +1,7 @@
 def Blocos(lista_conteudo_disco):
     lista_blocos = []
     for i in range(5536, 65536):
-        print(f'Leu o {i}')
+        # print(f'Leu o {i}')
         lista_blocos.append(lista_conteudo_disco[i])
 
     return lista_blocos
